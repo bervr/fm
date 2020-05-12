48c52f546e82edf46507eab62f27b0c1b0457b50
philippov <bervr@mail.ru>
2020-05-12 16:23:50 +0400

    first code

---
+++ code.py
@@ -0,0 +1,28 @@
+import os
+
+
+def create_file(name, text=None):
+    with open(name, 'w', encoding='utf-8') as f:
+        if text:
+            f.write(text)
+
+
+def create_folder(name):
+    try:
+        os.mkdir(name)
+    except FileExistsError:
+        print('Такая папка уже есть')
+
+
+def get_list(folders_only=False):
+    result = os.listdir()
+    if folders_only:
+        result = [f for f in result if os.path.isdir(f)]
+    print(result)
+
+
+if __name__ == '__main__':
+    create_file('123', 'some text')
+    create_folder('name1')
+    get_list()
+    get_list(True)
