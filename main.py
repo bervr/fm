9562b01dc63f16c6fe16b986a43ecb5a642f5b53
philippov <bervr@mail.ru>
2020-05-12 18:04:18 +0400

    main

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import sys
 from code import create_file, create_folder, delete_file, copy_file, save_info, get_list
 # get_list()
+save_info('Начало')
 command = sys.argv[1]
 if command== 'list':
     get_list()
 elif command == 'create_file':
-    name= sys.argv[2]
-    create_file(name)
+    try:
+        name= sys.argv[2]
+    except IndexError:
+        print('задайте имя файла')
+    else:
+        create_file(name)
 elif command == 'create_folder':
     name = sys.argv[2]
     create_folder(name)
@@ -18,10 +23,12 @@ elif command == 'copy':
     new_name = sys.argv[3]
     copy_file(name, new_name)
 elif command == 'help':
-    print(' list -список файлов или папок')
+    print('list -список файлов или папок')
     print('create_file - создание файла')
     print('create_folder - создание папки')
     print('delete - удаление файла или папки')
     print('copy - копирование файла или папки')
+
+save_info('Конец')
 
 
