7c20c7016d4e00fb2da16883f75fbbaab8c9c89b
philippov <bervr@mail.ru>
2020-05-12 16:50:31 +0400

    first code

--- code.py
+++ code.py
@@ -1,4 +1,7 @@
 import os
+import shutil
+import datetime
+
 
 
 def create_file(name, text=None):
@@ -21,8 +24,32 @@ def get_list(folders_only=False):
     print(result)
 
 
+def delete_file(name):
+    if os.path.isdir(name):
+        os.rmdir(name)
+    else:
+        os.remove(name)
+def copy_file(name,new_name):
+    if os.path.isdir(name):
+        try:
+            shutil.copytree(name,new_name)
+        except FileExistsError:
+            print('Такая папка уже есть')
+    else:
+        shutil.copy(name,new_name)
+def save_info(message):
+    current_time= datetime.datetime.now()
+    result = f'{current_time} - {message}'
+    with open ('log.txt', 'a', encoding='utf-8') as f:
+        f.write(result + '\n')
+
+
 if __name__ == '__main__':
-    create_file('123', 'some text')
-    create_folder('name1')
-    get_list()
-    get_list(True)
+    # create_file('123', 'some text')
+    # create_folder('name1')
+    # get_list()
+    # get_list(True)
+    # delete_file('123')
+    # copy_file('name1','name123')
+    # copy_file('123', 'name_1')
+    # save_info('rere')
